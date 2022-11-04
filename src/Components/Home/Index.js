904811d0ac5c4f7a0772f52655e88d1dbf09a3be
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2022-11-05 00:16:06 +0100

    redesign

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -4,11 +4,14 @@ import Login from '../Login/Index'
 import style from './home.module.css'
 import Intro from '../../Assets/Images/Intro.png'
 import woman from '../../Assets/Images/woman.png'
+import nature from '../../Assets/Images/nature.png'
+import balance from '../../Assets/Images/balance.png'
+import supervisor from '../../Assets/Images/supervisor.png'
 
 export default function Index() {
     return (
         <>
-            <div className='container'>
+            <div className={style.container}>
                 <div className={`${style['home-view']}`}>
                     <Header />
                     <img src={Intro} alt="tro-vest" className={style.intro} />
@@ -16,12 +19,27 @@ export default function Index() {
                 </div>
                 <Login />
             </div>
-            <div className='container' style={{
-                backgroundColor: '#F8F9FB'
-            }}>
+            <div className={style.container1}>
                 <img src={woman} alt="tro-vest-woman" className={style.woman} />
-                <div>
-                    
+                <div className={style.information}>
+                    <div className={style.informationtext}>
+                        <h3>Enjoy Saving Just the Way you know it.</h3>
+                        <h6>We are committed to always bring you wonderful deals on Various savings and investment scheme</h6>
+                    </div>
+                    <div className={style.trustgrid}>
+                        <div className={style.card}>
+                            <img src={nature} alt="tro-vest" className={style.trust} />
+                            <p className='saving'>Trusted</p>
+                        </div>
+                        <div className={style.card}>
+                            <img src={balance} alt="tro-vest" className={style.trust1} />
+                            <p className='saving'>Safe</p>
+                        </div>
+                        <div className={style.card}>
+                            <img src={supervisor} alt="tro-vest" className={style.trust2} />
+                            <p className='saving'>Reliable</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
