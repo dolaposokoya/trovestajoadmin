505cfa2bcbe33d946e16a27e8e4dc6829cc24cec
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2022-11-13 08:38:30 -0800

    update

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from '../Components/Home/Index'
 import Dashboard from '../Components/Dashboard/Index'
+import BroadCast from '../Components/BroadCast/Index'
 import { Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/dashboard" element={<Dashboard/>} />
+            <Route path="/bcm" element={<BroadCast/>} />
         </Routes>
     );
 }
