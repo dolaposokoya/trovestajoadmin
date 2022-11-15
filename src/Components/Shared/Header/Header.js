d412524686a8bfc2a0e8178f4efc83f7c18bec65
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2022-11-14 22:03:34 -0800

    Update

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import style from './header.module.css'
 import close from '../../../Assets/Images/close.png'
 import hamburger from '../../../Assets/Svg/hamburger.svg'
+import introsvg from '../../../Assets/Svg/trovest-finance-logo1.svg'
 
 export default function Header() {
     const [show, setshow] = useState(false)
@@ -20,6 +21,7 @@ export default function Header() {
                         <img src={close} className={style.close} />
                     </div>
                 </div>
+
                 <div className={`${style.header}`}>
                     <nav>
                         <ul>
@@ -34,6 +36,9 @@ export default function Header() {
             </div>
             {
                 <div className={style.harmburger}>
+                    <div className={style.harmburgerView}>
+                        <img src={introsvg} className={style.logo} />
+                    </div>
                     <div className={style.harmburgerView} onClick={() => changeIcon(true)}>
                         <img src={hamburger} className={style.hamburger1} />
                     </div>
