33f8ebbdfe42f296f4b9a924f83c70999b3674c1
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2022-11-20 18:05:45 +0100

    Agent update

--- src/Components/Dashboard/Agent.js
+++ src/Components/Dashboard/Agent.js
@@ -3,12 +3,11 @@ import styles from './agent.module.css'
 import style from './dashboard.module.css'
 import { useSelector } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom';
-import { convertToThousand, defaultImage, Naira } from '../../config';
-import Bell from '../../Assets/Svg/bell.svg'
+import { convertToThousand, defaultImage, dateFormat } from '../../config';
 import mail from '../../Assets/Svg/mail.svg'
-import ellipse from '../../Assets/Svg/ellipse.svg'
-import search from '../../Assets/Svg/search.svg'
-import plus from '../../Assets/Svg/plus.svg'
+import next from '../../Assets/Images/next.png'
+import nextwhite from '../../Assets/Images/nextwhite.png'
+import Card from '../Shared/Card/Card'
 
 
 export default function Agent() {
@@ -30,7 +29,8 @@ export default function Agent() {
     }
     return (
         <div className='container' style={{
-            flexDirection: 'column'
+            flexDirection: 'column',
+            marginBottom: '9em'
         }}>
             <div className={styles.profile}>
                 <img src={defaultImage} alt="profile-image" className='card' />
@@ -44,13 +44,12 @@ export default function Agent() {
             <div className={style.dashboardintro} style={{
                 width: '97%',
                 marginTop: '3em',
-                // padding: '.1em 0em',
             }}>
                 <div className={style.notification} style={{
                     marginRight: '1em',
                     marginTop: '.2em'
                 }}>
-                    <img src={mail} />
+                    <img src={mail} alt="mail" />
                 </div>
                 <div style={{
                     display: 'flex',
@@ -63,14 +62,56 @@ export default function Agent() {
                         <h3>Revenue Generated</h3>
                         <h5>{`${convertToThousand(agent?.amount || 0)}`}</h5>
                     </div>
-                    <div className={styles.profiledetails}>
-                        <h3 style={{
-                            marginTop: '1.3em'
-                        }}>Clients Onboarded</h3>
-                        <p>{agent?.artisans?.length || 0}</p>
+                    <div className={styles.profiledetails} style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}>
+                        <div className={styles.profiledetails1}>
+                            <h3 style={{
+                                marginTop: '1.3em'
+                            }}>Clients Onboarded</h3>
+                            <p>{agent?.artisans?.length || 0}</p>
+                        </div>
+                        <img src={nextwhite} alt="mail" />
                     </div>
                 </div>
             </div>
+            <div className={styles.transaction}>
+                <h3>Transaction Record</h3>
+                <img src={next} alt="mail" />
+            </div>
+            <div className={styles.transaction} style={{
+                justifyContent: 'center',
+                marginTop: '.5em'
+            }}>
+                <div className={styles.deposit}>
+                    <h3>Deposits</h3>
+                </div>
+                <div className={styles.withdrawn}>
+                    <h3>Withdrawn</h3>
+                </div>
+            </div>
+            <div className={styles.transaction1}>
+                <h3>Ref No</h3>
+                <h3>Amount</h3>
+            </div>
+            {agents?.map(item => (
+                <Card styles={style.tabledata} key={item._id} style={{
+                    width: '97%'
+                }}>
+                    <div className={style.details}>
+                        <div className={style.namedetails}>
+                            <h3>{`Trc2564876/34552242321`}</h3>
+                            <h3 className={style.date}>{dateFormat(item.createdAt)}</h3>
+                        </div>
+                    </div>
+                    <div className={style.amount}>
+                        <h3>{`${convertToThousand(8838292)}`}</h3>
+                    </div>
+                </Card>
+            ))}
         </div>
     )
 }
